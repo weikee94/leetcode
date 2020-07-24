f8ce91aeff0718edcaa7a474ad48d2bd9ca0093e
weikee94 <weikee070194@gmail.com>
2020-07-24 23:48:41 +0800

    141 linked list cycle

---
+++ linkedlist/141.linked-list-cycle.js
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=141 lang=javascript
+ *
+ * [141] Linked List Cycle
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+  let slow = head;
+  let fast = head;
+
+  while (fast !== null && fast.next !== null) {
+    fast = fast.next.next;
+    slow = slow.next;
+
+    if (fast === slow) {
+      return true;
+    }
+  }
+  return false;
+};
+// @lc code=end
+// 解法：
+// 龟兔赛跑again，快慢指针
