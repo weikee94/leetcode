f44933c928e36006b1ecbe498c73d474c826e8eb
weikee94 <weikee070194@gmail.com>
2020-08-03 22:08:02 +0800

    167 two sum ii input array is sorted

---
+++ array/167.two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=167 lang=javascript
+ *
+ * [167] Two Sum II - Input array is sorted
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (numbers, target) {
+  let returnArr = [];
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] === target) {
+        returnArr.push(i + 1);
+        returnArr.push(j + 1);
+      }
+    }
+  }
+  return returnArr;
+};
+// @lc code=end
+// 暴力解
+// 1. 两次循环
