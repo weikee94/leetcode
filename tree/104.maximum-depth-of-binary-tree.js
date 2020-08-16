691a6c60c75380c59a774ac3641c7a7505950c57
weikee94 <weikee070194@gmail.com>
2020-08-16 15:56:49 +0800

    104 maximum depth of binary tree with dfs approach

--- tree/104.maximum-depth-of-binary-tree.js
+++ tree/104.maximum-depth-of-binary-tree.js
@@ -18,31 +18,57 @@
  * @return {number}
  */
 var maxDepth = function (root) {
-  if (!root) {
-    return 0;
-  }
-  let queue = [root];
-  let depth = 0;
-  while (queue.length > 0) {
-    let len = queue.length;
-    depth++;
+  // 解法1
+  // if (!root) {
+  //   return 0;
+  // }
+  // let queue = [root];
+  // let depth = 0;
+  // while (queue.length > 0) {
+  //   let len = queue.length;
+  //   depth++;
 
-    for (let i = 0; i < len; i++) {
-      let currNode = queue.shift();
+  //   for (let i = 0; i < len; i++) {
+  //     let currNode = queue.shift();
 
-      if (currNode.left) {
-        queue.push(currNode.left);
-      }
-      if (currNode.right) {
-        queue.push(currNode.right);
+  //     if (currNode.left) {
+  //       queue.push(currNode.left);
+  //     }
+  //     if (currNode.right) {
+  //       queue.push(currNode.right);
+  //     }
+  //   }
+  // }
+  // return depth;
+
+  // 解法2:
+  if (!root) return 0;
+  let max = -Infinity;
+
+  var dfs = function (node, depth) {
+    if (!node.left && !node.right) {
+      if (depth > max) {
+        max = depth;
       }
     }
-  }
-  return depth;
+    if (node.left) {
+      dfs(node.left, depth + 1);
+    }
+    if (node.right) {
+      dfs(node.right, depth + 1);
+    }
+  };
+  dfs(root, 1);
+  return max;
 };
 // @lc code=end
 
-// 解法：
+// 解法1：
 // 1. 使用stack || queue
 // 2. 根据当前node，判断左右子树，依次加入stack || queue
 // 3. 建立一个空数组记录遍历顺序
+
+// 解法2: dfs
+// 1. recursion
+// 2. process current lv
+// 3. drill down
