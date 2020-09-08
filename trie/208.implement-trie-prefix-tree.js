6687553183b85a6f5b4d80908a57ee57587467fc
weikee94 <weikee070194@gmail.com>
2020-09-08 22:47:20 +0800

    208 implement trie prefix tree

--- trie/208.implement-trie-prefix-tree.js
+++ trie/208.implement-trie-prefix-tree.js
@@ -1,50 +1,69 @@
-/*
- * @lc app=leetcode id=208 lang=javascript
- *
- * [208] Implement Trie (Prefix Tree)
- */
-
-// @lc code=start
 /**
  * Initialize your data structure here.
  */
-var Trie = function() {
-    
+var Trie = function () {
+  this.root = {};
 };
 
 /**
- * Inserts a word into the trie. 
+ * Inserts a word into the trie.
  * @param {string} word
  * @return {void}
  */
-Trie.prototype.insert = function(word) {
-    
+Trie.prototype.insert = function (word) {
+  let node = this.root;
+  for (let c of word) {
+    if (!node[c]) {
+      node[c] = {};
+    }
+    node = node[c];
+  }
+  // 这里是为了标记最后一个字母是什么
+  // example "ap"
+  // {a: {p: {isWord: {true}}}}
+  node.isWord = true;
 };
 
 /**
- * Returns if the word is in the trie. 
+ * Returns if the word is in the trie.
  * @param {string} word
  * @return {boolean}
  */
-Trie.prototype.search = function(word) {
-    
+Trie.prototype.search = function (word) {
+  const node = this.traverse(word);
+  return node !== null && node.isWord === true;
+};
+
+Trie.prototype.traverse = function (word) {
+  let node = this.root;
+  for (let c of word) {
+    if (node[c]) {
+      node = node[c];
+      // 每次遍历都进去下一层
+      // {a: {p: {isWord: {true}}}} -> {p: {isWord: {true}}} -> isWord: {true}
+    } else {
+      return null;
+    }
+  }
+  return node;
 };
 
 /**
- * Returns if there is any word in the trie that starts with the given prefix. 
+ * Returns if there is any word in the trie that starts with the given prefix.
  * @param {string} prefix
  * @return {boolean}
  */
-Trie.prototype.startsWith = function(prefix) {
-    
+Trie.prototype.startsWith = function (prefix) {
+  const node = this.traverse(prefix);
+  return node !== null;
 };
 
-/** 
+/**
  * Your Trie object will be instantiated and called as such:
  * var obj = new Trie()
  * obj.insert(word)
  * var param_2 = obj.search(word)
  * var param_3 = obj.startsWith(prefix)
  */
-// @lc code=end
 
+// 1. 这个记得模版即可 🤖
