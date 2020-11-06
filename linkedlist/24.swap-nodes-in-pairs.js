0a41852e784062681587e5939f0474900dbb8ff4
weikee94 <weikee070194@gmail.com>
2020-11-06 15:18:45 +0800

    24 swap nodes in pairs

--- linkedlist/24.swap-nodes-in-pairs.js
+++ linkedlist/24.swap-nodes-in-pairs.js
@@ -17,7 +17,30 @@
  * @return {ListNode}
  */
 var swapPairs = function(head) {
+    if(!head || !head.next) {
+        return head;
+    }
     
+    // 当前node
+    var cur = head;
+    
+    // 当前node 下一个值
+    var nextCur = head.next;
+    
+    // 当换了前两个，我们要从第三个开始
+    var nextNextCur = head.next.next;
+    
+    // 把当前node下一个值的指针指向当前node, 既可以完成 [2,1,2,3,4]
+    nextCur.next = cur;
+    
+    // 把当前node 下一个指针指向 [3,4] 传回来的值, 也就是head.next.next;
+    cur.next = swapPairs(nextNextCur);
+    
+    return nextCur;
 };
+
 // @lc code=end
+// 解题思路
+// 这题一开始有想法可是实现不出来，需要review again
+// 1. 每次置换后，把需要转换的数组传进递归里 
 
