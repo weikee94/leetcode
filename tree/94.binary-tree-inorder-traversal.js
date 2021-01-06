a3956219c8a74cc28761dee39432ad49084261d1
weikee94 <weikee070194@gmail.com>
2021-01-06 21:58:37 +0800

    add binary tree inorder traversal iterative approach

--- tree/94.binary-tree-inorder-traversal.js
+++ tree/94.binary-tree-inorder-traversal.js
@@ -41,3 +41,35 @@ var inorderTraversal = function (root) {
 //   self.inorder(root.right)
 
 // 尝试 iterative approach
+var inorderTraversal = function(root) {
+  // 维护 stack
+  // 检查边界条件
+  if(!root) {
+      return [];
+  }
+  
+  let stack = [];
+  let result = [];
+  let current = root;
+  
+  // stack length 是终止条件
+  // 又或者 current 已经是 null (想象3的时候，已经是最后一个node，current = current.right 
+  // 相当于 current = null 就跳出循环了)
+  /**
+  *           1
+  *         /   \
+  *        2     3
+  */
+  while(current !== null || stack.length) {
+      if(current) {
+          stack.push(current)
+          current = current.left
+      } else {
+          current = stack.pop()
+          result.push(current.val)
+          current = current.right;
+      }
+  }
+  
+  return result;
+}
