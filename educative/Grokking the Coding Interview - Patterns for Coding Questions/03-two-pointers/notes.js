ebc5faf68410c4562c105bfcb4e1c766b09390c1
wkee0701 <13563834+weikee94@users.noreply.github.com>
2021-04-08 09:31:24 +0800

    Remove Duplicates and Remove all instance with key

--- educative/Grokking the Coding Interview - Patterns for Coding Questions/03-two-pointers/notes.js
+++ educative/Grokking the Coding Interview - Patterns for Coding Questions/03-two-pointers/notes.js
@@ -22,3 +22,26 @@ while(left < right) {
 	}
 }
 console.log('result: ', result)
+
+// 第二题
+// Remove Duplicates
+let arr = [2,3,3,3,6,9,9];
+let nextNonDuplicate = 1;
+for(let i = 1; i < arr.length; i++) {
+	if(arr[nextNonDuplicate - 1] !== arr[i]) {
+		arr[nextNonDuplicate] = arr[i];
+		nextNonDuplicate++;
+	}
+}
+
+console.log("nextNonDuplicate: ", nextNonDuplicate)
+
+// Remove all instance with key
+let arr = [3,2,3,6,3,10,9,3], key = 3
+let notTargetNum = 0;
+for(let i = 0; i < arr.length; i++) {
+	if(arr[i] !== key) {
+		notTargetNum++;
+	}
+}
+console.log('notTargetNum: ', notTargetNum)
