000c4df5aaeef1b75656adc475a40b2c7dd6b3f9
wkee0701 <13563834+weikee94@users.noreply.github.com>
2021-09-26 09:09:27 +0800

    feat: update qn 33

--- binary-search/33.search-in-rotated-sorted-array.js
+++ binary-search/33.search-in-rotated-sorted-array.js
@@ -76,3 +76,44 @@ var search = function (nums, target) {
 
   return -1;
 };
+
+
+// 26/09/2021
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+ var search = function(nums, target) {
+  let left = 0, right = nums.length - 1, result = -1;
+  
+  while(left <= right) {
+      let mid = left + (Math.floor((right-left)/2));
+      
+      if(nums[mid] === target) {
+          return mid;
+      }
+      
+      // check whether left side is sorted
+      if(nums[left] <= nums[mid]) {
+          // check whether the target is in between nums[left] and nums[mid]
+          if(target >= nums[left] && target <= nums[mid]) {
+              right = mid - 1
+          } else {
+              left = mid + 1;
+          }
+      } 
+      
+      // otherwise right side is sorted
+      else {
+          if(target >= nums[mid] && target <= nums[right]) {
+              left = mid + 1
+          } else {
+              right = mid - 1;
+          }
+      }
+      
+  }
+  
+  return result;
+};
