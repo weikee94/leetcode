f5a1e36ded45c80fa0562e5e42096db9b5a00086
wkee0701 <13563834+weikee94@users.noreply.github.com>
2021-09-28 20:34:34 +0800

    feat: update another attempt for qn 153

--- binary-search/153.find-minimum-in-rotated-sorted-array.js
+++ binary-search/153.find-minimum-in-rotated-sorted-array.js
@@ -32,3 +32,32 @@ var findMin = function (nums) {
 
 // 解法 2
 // 2. 二分查找
+
+
+
+// 28/09/2021
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var findMin = function(nums) {
+  let left = 0;
+  let right = nums.length - 1;
+  
+  // 这里判断为什么不能用等于？？
+  while(left < right) {
+      let mid = Math.floor((left) + (right-left)/2);
+      
+      if(nums[mid] > nums[right]) {
+          left = mid + 1
+      } else {
+          right = mid
+      }
+  }
+  
+  return nums[left]
+};
+
+// 可能会有的疑惑
+// - nums[mid] > nums[right]为什么肯定是 left = mid + 1, 因为假设旋转过后第一个要排在right 的后面那么必定是比right 大，不可能出现同时mid > right 且最小数在left 
